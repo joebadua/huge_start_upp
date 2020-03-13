f5d6f531c15cb182a9430081d7019fb344cf533f
joebadua <joebadqq@gmail.com>
2020-03-12 20:03:11 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ function App() {
     <div className="App">
       <h1>huge start UP</h1>
       <h2><i>huge</i></h2>
+      <section id="headers">
+        <h2>CRO</h2>
+        <img id="pp" src="0.jpeg"></img>
+        <h1></h1>
+        <h3>Frank C.</h3>
+
+        <h2 id="head">CFO</h2>
+        <img id="pp" src="0-2.jpeg"></img>
+        <h3>Jon S.</h3>
+
+        <h2 id="head">CEO</h2>
+        <img id="pp" src="0-3.jpg"></img>
+        <h3>Joe B.</h3>
+      </section>
     </div>
   );
 }
